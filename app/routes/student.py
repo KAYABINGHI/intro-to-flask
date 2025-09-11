a42a2b2083b751cb52ba9bb5c3249f846ea5413d
KAYABINGHI <muchaipauls@gmail.com>
2025-09-11 11:02:58 +0300

    updated data

--- app/routes/student.py
+++ app/routes/student.py
@@ -4,14 +4,23 @@ from flask import Blueprint,jsonify,request
 student_bp=Blueprint("student",__name__)
 
 
-@student_bp.route("/",methods=["GET","POST"])
+@student_bp.route("/",methods=["GET"])
 def single_student():
     print("Single student")
     return "Single student"
 
+#adding a student to our db
 #routes and controller logic
-@student_bp.route("/add",methods=["POST"])
-def add_user():
+@student_bp.route("/add/json",methods=["POST"])
+def add_user_json():
+    print("Add user was hit")
+    data=request.get_json() #Body to json as python dic
+    print("Received Data",data)
+    # Read the request
+    return "Adding a student",200
+
+@student_bp.route("/add/form",methods=["POST"])
+def add_user_form():
     print("Add user was hit")
     return "Adding a student",200
 
--- migrations/versions/77e9a441c3cf_initial_migration.py
+++ migrations/versions/77e9a441c3cf_initial_migration.py
@@ -1,8 +1,8 @@
-"""initial
+"""initial migration
 
-Revision ID: fbd95e266b09
+Revision ID: 77e9a441c3cf
 Revises: 
-Create Date: 2025-09-08 12:14:39.201469
+Create Date: 2025-09-11 09:58:06.419171
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'fbd95e266b09'
+revision = '77e9a441c3cf'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,7 +21,10 @@ def upgrade():
     op.create_table('student',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
-    sa.PrimaryKeyConstraint('id')
+    sa.Column('email', sa.String(length=500), nullable=False),
+    sa.Column('created_at', sa.DateTime(timezone=True), nullable=False),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('email')
     )
     # ### end Alembic commands ###
 
